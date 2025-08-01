82e288d65cdc2ea84f2692590c8fe1be0d6e4f3a
AlecJDavidson <alecjdavidson@outlook.com>
2025-08-01 16:16:56 -0400

    Quick and dirty brick creation. I'll come back to this later.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import BrickTable from './components/BrickTable';
 import BrickDetails from './components/BrickDetails';
+import CreateBrick from './components/CreateBrick';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<BrickTable />} />
-        <Route path="/brick-details/:brickId" element={<BrickDetails />} />
+        <Route path='/' element={<BrickTable />} />
+        <Route path='/brick-details/:brickId' element={<BrickDetails />} />
+        <Route path='/create-brick' element={<CreateBrick />} />
       </Routes>
     </Router>
   );
